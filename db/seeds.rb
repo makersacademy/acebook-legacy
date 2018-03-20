89bc1c335bf9e149a815329fa24ec84f7d4f3781
Edward Withers <dearshrewdwit@gmail.com>
2018-03-20 18:39:40 +0000

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,5 @@
 # Add seeds here
+
+User.create(name: 'Albus Dumbledore', email: 'alby@hogwarts.com', password: 'gellert')
+User.create(name: 'Agathe Christie', email: 'aggiewaggy@romance_novels.com', password: 'cadburys')
+User.create(name: 'Achilles', email: 'chillychillz@anachronism.com', password: 'sticks')
